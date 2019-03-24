c73d692d13bbb5a41085c2023bbca980c56a1cef
Crowdbotics <team@crowdbotics.com>
2019-03-24 20:24:41 +0000

    initial commit

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: waitress-serve --port=$PORT backend.maintenance_data_man_1636.wsgi:application
+release: ./backend/release-tasks.sh
---
+++ backend/maintenance_data_man_1636/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for maintenance_data_man_1636 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "maintenance_data_man_1636.settings")
+
+application = get_wsgi_application()
